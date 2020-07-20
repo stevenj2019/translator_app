6c7da6812b79d14c922ea8e0ae32d1be1d682c2e
Steven Johnson <steven.j2019@pm.me>
2020-07-20 15:15:00 +0100

    changes

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template
+from os import getenv
+import uuid, json, requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+
+client = SecretClient(vault_url=https://translateappenvvars.vault.azure.net/, credential=DefaultAzureCredential())
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY']=client.get_secret('SECRET_KEY')
+
+client = SecretClient(vault_url= , credential=DefaultAzureCredential() )
+
+langs = requests.get('https://api.cognitive.microsofttranslator.com/languages?api-version=3.0')
+langs = langs.json()
+languages = []
+for lang in langs['translation']:
+    languages.append((lang,langs['translation'][lang]['nativeName']))
+
+class TranslateForm(FlaskForm):
+
+    sentence = StringField('Enter Some Text')
+    language = SelectField('Language',
+        choices=languages)
+    submit = SubmitField('Translate')
+
+@app.route('/', methods=['GET', 'POST'])
+def translate():
+    form = TranslateForm()
+    response = ""
+    if form.validate_on_submit():
+        subscription_key = client.get_secret('TRANSLATOR_TEXT_SUBSCRIPTION_KEY')
+        endpoint = client.get_secret('TRANSLATOR_TEXT_ENDPOINT')
+        addition = "translate?api-version=3.0&to="
+        headers = {
+        'Ocp-Apim-Subscription-Key':subscription_key,
+        'Ocp-Apim-Subscription-Region':'westeurope',
+        'Content-Type':'application/json'
+        }
+        body = [{
+        'Text': form.sentence.data
+        }]
+        constructed_url = endpoint + addition + form.language.data
+        request = requests.post(constructed_url, headers=headers, json=body)
+        response = request.json()
+        response = response[0]['translations'][0]['text']
+    return render_template('index.html', form=form, translation=response)
+
+
+if __name__=='__main__':
+    app.run(debug=True)
